3ba6abb5c0d2b708a20fb9f6f782bb9d71ffb014
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2026-08-20 18:53:36 +0530

    Variables, Let, const.

---
+++ javascript/week-01/day-01/profile.js
@@ -0,0 +1,21 @@
+let firstName = "Saurabh";
+let lastName = "Sindhu";
+let age = "30";
+let country = "India";
+let city = "New Delhi";
+let currentGoal = "Excellent Software Engineer";
+
+console.log(
+  `Hi, I'm ${firstName}. I'm ${age} years old and currently working toward becoming a ${currentGoal}.`
+); // Template literal
+
+console.log(
+  "Hi, I'm " +
+    firstName +
+    ". I'm " +
+    age +
+    " years old and currently working toward becoming a" +
+    currentGoal
+);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- javascript/week-01/day-01/variables.js
+++ javascript/week-01/day-01/variables.js
@@ -1,1 +1,38 @@
-h;
+/* 
+//Problem
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+
+/* 
+//Solution
+let admin;
+let name = "John";
+
+admin = name;
+console.log(admin);
+
+*/
+
+///////////////////////////////////////////////////////
+const firstName = "Saurabh";
+let age = "30";
+let profession = "Software Engineer";
+
+console.log(firstName, age, profession);
+///////////////////////////////////////////////////////
+age = "31";
+console.log(age); //When the value is changed the old data is removed from the variable.
+////////////////////////////////////////////////////////
+
+const country = "India";
+
+console.log(country);
+
+///// country = "Canada";
+
+console.log(country); // Here we are getting error because the const is a fixed variable and we are trying to change it's value. So we need to use let instead of const if we want to change a value or declare a new variable instead.
+
+///////////////////////////////////////////////////////////////
