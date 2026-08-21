32c133c6d0c35938e1f5616ad67055c3e141008e
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2026-08-21 23:08:08 +0530

    Data Types

---
+++ javascript/week-01/day-01/datatypes.js
@@ -0,0 +1,32 @@
+// String
+const name = "Saurabh";
+
+// Number
+const age = 30;
+const height = 180.5;
+
+// Boolean
+const isLearningJavaScript = true;
+
+// Undefined
+let futureJob;
+
+// Null
+const currentJob = null;
+
+console.log(typeof name);
+console.log(typeof futureJob);
+console.log(typeof isLearningJavaScript);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*let name = "Ilya";
+
+// the expression is a number 1
+alert(`hello ${1}`); // hello 1
+
+// the expression is a string "name"
+alert(`hello ${"name"}`); // hello name
+
+// the expression is a variable, embed it
+alert(`hello ${name}`); // hello Ilya */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
