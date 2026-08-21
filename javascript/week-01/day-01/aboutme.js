663900ecae310464f35c23d64adc83bae5a85356
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2026-08-21 23:52:44 +0530

    Complete JavaScript Day 1 fundamentals

---
+++ javascript/week-01/day-01/aboutme.js
@@ -0,0 +1,30 @@
+// Final Challenge
+/*Output should be : Hi, I'm Saurabh Sindhu.
+I'm 30 years old and currently based in ______.
+I'm currently learning JavaScript.
+My target role is Software Engineer.
+I have ___ years of professional experience. */
+//////////////////////////////////////////////////////
+const firstName = "Saurabh";
+const lastName = "Sindhu";
+let age = 30;
+let city = "Vancouver";
+let country = "Canada";
+let isLearningJavaScript = true;
+let yearsOfExperience = 3;
+let targetRole = "Software Engineer";
+
+console.log(`Hi, I'm ${firstName} ${lastName}.`);
+console.log(`I'm ${age} years old and currently based in ${city}.`);
+console.log(`I'm currently learning JavaScript.`);
+console.log(`My target role is ${targetRole}.`);
+console.log(`I have ${yearsOfExperience} years of professional experience.`);
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof country);
+console.log(typeof isLearningJavaScript);
+console.log(typeof yearsOfExperience);
+console.log(typeof targetRole);
---
+++ javascript/week-01/day-01/type-conversion.js
@@ -0,0 +1,15 @@
+/*const age = "30";
+
+//console.log(age);
+//console.log(typeof age);
+
+const convertedAge = Number(age);
+
+console.log(convertedAge);
+console.log(typeof convertedAge); */
+
+console.log("5" + 2); //52
+console.log("5" - 2); // 3
+
+console.log("5" + true); // 5true
+console.log("5" - true); // 4
