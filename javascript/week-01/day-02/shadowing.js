87d290756f13172a5367fd7b188aa4b6de635d2e
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2026-08-30 17:33:45 +0530

    Shadowing

---
+++ javascript/week-01/day-02/shadowing.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// ============================================
+// DAY 2 — VARIABLE SHADOWING
+// ============================================
+
+const name = "Saurabh";
+
+function greet() {
+  const name = "John";
+
+  console.log(name);
+}
+
+greet();
+
+console.log(name);
+
+// Output:
+//
+// John
+// Saurabh
